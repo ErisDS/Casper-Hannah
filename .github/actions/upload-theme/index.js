a8558ffd864d20bb3a93443e0a5534df6f610330
Hannah Wolfe <erisds@gmail.com>
2019-10-10 16:45:57 +0100

    something new

--- .github/actions/upload-theme/index.js
+++ .github/actions/upload-theme/index.js
@@ -2,28 +2,32 @@ const core = require('@actions/core');
 const GhostAdminApi = require('@tryghost/admin-api');
 
 
-console.log('zip-path', core.getInput('zip-path'));
-const pkgPath = `${process.pwd()}/package.json`;
-console.log('cwd', process.pwd(), pkgPath);
-const pkg = require(pkgPath);
-console.log('pkg', pkg.name);
+(async function main() {
 
-const api = new GhostAdminApi({
-    url: core.getInput('api-url'),
-    key: core.getInput('api-key'),
-    version: 'canary'
-});
+    console.log('zip-path', core.getInput('zip-path'));
+    const pkgPath = `${process.cwd()}/package.json`;
+    console.log('cwd', process.cwd(), pkgPath);
+    const pkg = require(pkgPath);
+    console.log('pkg', pkg.name);
 
-const zipPath = core.getInput('zip-path');
-
-console.log(zipPath);
-
-api.themes
-    .upload({file: zipPath})
-    .then(() => {
-        console.log('SUCCESS');
-    })
-    .catch((err) => {
-        console.error(err);
-        process.exit(1);
+    const api = new GhostAdminApi({
+        url: core.getInput('api-url'),
+        key: core.getInput('api-key'),
+        version: 'canary'
     });
+
+    const zipPath = core.getInput('zip-path');
+
+    console.log(zipPath);
+
+    // api.themes
+    //     .upload({file: zipPath})
+    //     .then(() => {
+    //         console.log('SUCCESS');
+    //     })
+    //     .catch((err) => {
+    //         console.error(err);
+    //         process.exit(1);
+    //     });
+
+}())
